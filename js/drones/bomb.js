562d09e630c373ada4493c204f2b031827af8bd2
YuriiKomaniak1 <human8555@gmail.com>
2025-04-13 12:40:28 +0300

    Update bomb.js

--- js/drones/bomb.js
+++ js/drones/bomb.js
@@ -1,12 +1,12 @@
 import { basePath } from "../utils/basePath.js`;
 let fragBombImage = new Image();
-fragBombImage.src = `${basePath}/assets/img/bombs/fragBomb.png`;
+fragBombImage.src = `${basePath}assets/img/bombs/fragBomb.png`;
 let heBombImage = new Image();
-heBombImage.src = `${basePath}/assets/img/bombs/heBomb.png`;
+heBombImage.src = `${basePath}assets/img/bombs/heBomb.png`;
 let shapedBombImage = new Image();
-shapedBombImage.src = `${basePath}/assets/img/bombs/shapedBomb.png`;
+shapedBombImage.src = `${basePath}assets/img/bombs/shapedBomb.png`;
 let imageExplosion = new Image();
-imageExplosion.src = `${basePath}/assets/img/bombs/smallExplosion.png`;
+imageExplosion.src = `${basePath}assets/img/bombs/smallExplosion.png`;
 import { switchToNextAvailableBomb } from "../logic/controls.js";
 export class Bomb {
   static weight = 0.1; // За замовчуванням
